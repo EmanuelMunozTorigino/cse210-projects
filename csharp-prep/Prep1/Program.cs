85b21529e1f7738f7b063ab8a0e2fc1859599f78
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-03-02 19:55:45 -0300

    Practice with conditionals in C#

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -17,6 +17,44 @@ class Program
 
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ");
 
+
+
+        Console.WriteLine("What was your grade? ");
+        string grade = Console.ReadLine();
+        gradeNumber = int.Parse(grade)
+
+        if (gradeNumber >= 90)
+        {
+            Console.WriteLine("You get an 'A'");
+        }
+        elif(gradeNumber >= 80)
+            {
+            Console.WriteLine("You get a 'B'");
+        }
+
+        elif(gradeNumber >= 70)
+        {
+            Console.WriteLine("You get a 'C'");
+        }
+
+        elif(gradeNumber >= 60)
+            {
+            Console.WriteLine("You get a 'D'");
+        }
+
+        elif(gradeNumber < 60)
+            {
+            Console.WriteLine("You get a 'F'");
+        }
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("You passed the class");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, try next semester, you can do it");
+        }
     }
 
 
