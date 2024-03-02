ba5f21312b8ff984a25a57a40088ae35f104eac4
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-03-02 20:48:11 -0300

    Practicing with conditionals statements

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -16,46 +16,8 @@ class Program
         string lastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ");
-
-
-
-        Console.WriteLine("What was your grade? ");
-        string grade = Console.ReadLine();
-        gradeNumber = int.Parse(grade)
-
-        if (gradeNumber >= 90)
-        {
-            Console.WriteLine("You get an 'A'");
-        }
-        elif(gradeNumber >= 80)
-            {
-            Console.WriteLine("You get a 'B'");
-        }
-
-        elif(gradeNumber >= 70)
-        {
-            Console.WriteLine("You get a 'C'");
-        }
-
-        elif(gradeNumber >= 60)
-            {
-            Console.WriteLine("You get a 'D'");
-        }
-
-        elif(gradeNumber < 60)
-            {
-            Console.WriteLine("You get a 'F'");
-        }
-
-        if (gradeNumber >= 70)
-        {
-            Console.WriteLine("You passed the class");
-        }
-        else
-        {
-            Console.WriteLine("Sorry, try next semester, you can do it");
-        }
     }
+
 
 
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,79 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //        Console.WriteLine("Hello Prep2 World!");
+
+
+        Console.WriteLine("What was your grade? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+
+        int remainderGrade = gradeNumber % 10;
+
+
+        string sign = "";
+
+        string letter = "";
+
+        // Check the score and its relatives letter grade
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (gradeNumber >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (gradeNumber >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (gradeNumber < 60)
+        {
+            letter = "F";
+        }
+
+        // Verify if the letter has a - or + sign after grade letter
+
+        if (remainderGrade >= 7)
+        {
+            sign = "+";
+        }
+        else if (remainderGrade < 3)
+        {
+            sign = "-";
+        }
+
+
+        // There is not - or + sign for an "A" or 'F' grade, so its ommits the sign or restore its value to an empty string
+        if (letter == "A" || letter == "F")
+        {
+            sign = "";
+        }
+
+        // print the score following to its sign if 
+        Console.WriteLine($"You get a '{letter}{sign}'");
+
+
+        // A congrats message or an encourage message to next term is display!
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("You passed the class");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, try next semester, you can achieve it");
+        }
+
+
     }
 }
